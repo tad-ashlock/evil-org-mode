e85f79c9c09b71650e7912672037f4561f5eba6a
Tad Ashlock <tad.ashlock@gmail.com>
2014-10-05 04:59:57 -0600

    Add proof-of-concept implementation with Evil-Mode integration hooks
    Evil-Mode has been modified to call some new hook functions instead
    of directly calling certain low-level elisp functions.  This allows
    us to, for example, insert a call to org-beginning-of-line into the
    evil-beginning-of-line function.  This integration is nowhere near
    complete.

--- evil-org.el
+++ evil-org.el
@@ -1,12 +1,12 @@
-;;; evil-org.el --- evil keybindings for org-mode
+;;; evil-org-mode.el --- evil keybindings for org-mode
 
-;; Copyright (C) 2012-2013 by Edward Tjörnhammar
-;; Author: Edward Tjörnhammar
-;; URL: https://github.com/edwtjo/evil-org-mode.git
-;; Git-Repository; git://github.com/edwtjo/evil-org-mode.git
-;; Created: 2012-06-14
-;; Version: 0.1.1
-;; Package-Requires: ((evil "0") (org "0"))
+;; Copyright (C) 2014 by Tad Ashlock
+;; Author: Tad Ashlock
+;; URL: https://github.com/tadashlock/evil-org-mode.git
+;; Git-Repository; git://github.com/tadashlock/evil-org-mode.git
+;; Created: 2014-08-23
+;; Version: 0.1.0
+;; Package-Requires: ((evil "0")) ;TODO
 ;; Keywords: evil vim-emulation org-mode key-bindings presets
 
 ;; This file is not part of GNU Emacs
@@ -24,126 +24,178 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
-;;
 ;; Known Bugs:
-;; See, https://github.com/edwtjo/evil-org-mode/issues
+;; See, https://github.com/tadashlock/evil-org-mode/issues
+
+;; TODO:
+;; * Fix org-mode to not have hard-coded references to key sequences in messages / documentation.
+;;   For example, in org-kill-line, it mentions 'C-k' in an error message.
 ;;
-;;; Code:
+;; * Maybe add to org-mode the function 'org-kill-whole-line' to override Emacs's 'kill-whole-line'.
+;;   See 'org-kill-line' for reasons why.
+;;
+;; * Change org-mode's org-special-ctrl-a/e variable to not be key binding specific.
+;;
+;; * Change org-support-shift-left to not be key binding specific.
+;;
+;; * It looks to me like the mapping of evil-digit-argument-or-evil-beginning-of-line to "0"
+;;   in evil-integration.el should actually call evil-define-motion evil-beginning-of-line-or-digit-argument.
+
 (require 'evil)
 (require 'org)
 
 (define-minor-mode evil-org-mode
-  "Buffer local minor mode for evil-org"
+  "Buffer-local minor mode for integrating Evil Mode and Org Mode"
   :init-value nil
   :lighter " EvilOrg"
   :keymap (make-sparse-keymap) ; defines evil-org-mode-map
-  :group 'evil-org)
+  :group 'evil-org
+  (if evil-org-mode
+      (evil-org-register)
+    (evil-org-unregister)))
 
-(add-hook 'org-mode-hook 'evil-org-mode) ;; only load with org-mode
+(add-hook 'org-mode-hook 'evil-org-mode) ; only load with org-mode
 
-(defun clever-insert-item ()
-  "Clever insertion of org item."
-  (if (not (org-in-item-p))
-      (insert "\n")
-    (org-insert-item))
-  )
 
-(defun evil-org-eol-call (fun)
-  "Go to end of line and call provided function.
-FUN function callback"
-  (end-of-line)
-  (funcall fun)
-  (evil-append nil)
-  )
+(defun evil-org-register ()
+  (add-hook 'evil-move-beginning-of-line--move-beginning-of-line-functions
+            'evil-org-beginning-of-line)
+  (add-hook 'evil-beginning-of-line--move-beginning-of-line-functions
+            'evil-org-beginning-of-line)
+  (add-hook 'evil-delete-region--delete-region-functions
+            'evil-org-delete-region)
+  (add-hook 'evil-delete-backward-char-and-join--delete-backward-char-functions
+            'evil-org-delete-backward-char-and-join))
 
-;; recompute clocks in visual selection
-(evil-define-operator evil-org-recompute-clocks (beg end type register yank-handler)
-  :keep-visual t
-  :move-point nil
-  (interactive "<r>")
-  (progn
-        (message "start!" )
-        (save-excursion
-        (while (< (point) end)
-            (org-evaluate-time-range)
-            (next-line)
-            (message "at position %S" (point))
-        ))))
+(defun evil-org-unregister ()
+  (remove-hook 'evil-move-beginning-of-line--move-beginning-of-line-functions
+               'evil-org-move-beginning-of-line)
+  (remove-hook 'evil-beginning-of-line--move-beginning-of-line-functions
+               'evil-org-beginning-of-line)
+  (remove-hook 'evil-delete-region--delete-region-functions
+               'evil-org-delete-region)
+  (remove-hook 'evil-delete-backward-char-and-join--delete-backward-char-functions
+               'evil-org-delete-backward-char-and-join))
 
-;; open org-mode links in visual selection
-(evil-define-operator evil-org-open-links (beg end type register yank-handler)
-  :keep-visual t
-  :move-point nil
-  (interactive "<r>")
-  (progn
-        (message "start of evil-org-open-links 2" )
-        (save-excursion
-          (goto-char beg)
-          (beginning-of-line)
-          (catch 'break
-          (while (< (point) end)
-            (message "at position %S" (point))
-            (org-next-link)
-            (when (not(< (point) end)) (throw 'break 0))
-            (org-open-at-point)
-            )))))
 
-;; normal state shortcuts
+;;; Motions
+
+(defun org-forward-paragraph-new (count)
+  (interactive)
+  (org-forward-paragraph))
+
+(defun org-backward-paragraph-new (count)
+  (interactive)
+  (org-backward-paragraph))
+
+(evil-define-motion evil-org-forward-paragraph (count)
+  "Move to the end of the COUNT-th next paragraph."
+  :jump t
+  :type exclusive
+  (evil-move-end count #'org-forward-paragraph-new #'org-backward-paragraph-new))
+
+(define-key evil-org-mode-map [remap evil-forward-paragraph] #'evil-org-forward-paragraph)
+
+
+
+;;;
+
+(defun evil-org-beginning-of-line (arg)
+  (org-beginning-of-line arg)
+  t)
+
+; This function calls org-delete-char with an argument of 1 rather than N in order
+; to get the advanced table handling of Org Mode.
+(defun evil-org-delete-region (beg end)
+  (let ((N (- end beg)))
+    (save-excursion
+      (goto-char beg)
+      (dotimes (i N) (org-delete-char 1))))
+  t)
+
+
+(evil-define-command evil-org-delete-backward-char-and-join (count)
+  "Delete previous character and join lines.
+If point is at the beginning of a line then the current line will
+be joined with the previous line if and only if
+`evil-backspace-join-lines'."
+  (interactive "p")
+  (org-delete-backward-char count)
+  t)
+
+
+
 (evil-define-key 'normal evil-org-mode-map
-  "gh" 'outline-up-heading
-  "gj" (if (fboundp 'org-forward-same-level) ;to be backward compatible with older org version
-	   'org-forward-same-level
-	  'org-forward-heading-same-level)
-  "gk" (if (fboundp 'org-backward-same-level)
-	   'org-backward-same-level
-	  'org-backward-heading-same-level)
-  "gl" 'outline-next-visible-heading
-  "t" 'org-todo
-  "T" '(lambda () (interactive) (evil-org-eol-call (lambda() (org-insert-todo-heading nil))))
-  "H" 'org-beginning-of-line
-  "L" 'org-end-of-line
-  "o" '(lambda () (interactive) (evil-org-eol-call 'clever-insert-item))
-  "O" '(lambda () (interactive) (evil-org-eol-call 'org-insert-heading))
-  "$" 'org-end-of-line
-  "^" 'org-beginning-of-line
-  "<" 'org-metaleft
-  ">" 'org-metaright
-  "-" 'org-cycle-list-bullet
-  (kbd "TAB") 'org-cycle)
+ "[\t" 'org-cycle
+ "[C" 'org-ctrl-c-ctrl-c
+ "[\r" 'org-ctrl-c-ret)
 
-;; leader maps
-(evil-leader/set-key-for-mode 'org-mode
-  "t"  'org-show-todo-tree
-  "a"  'org-agenda
-  "c"  'org-archive-subtree
-  "l"  'evil-org-open-links
-  "o"  'evil-org-recompute-clocks
-)
 
-;; normal & insert state shortcuts.
-(mapc (lambda (state)
-        (evil-define-key state evil-org-mode-map
-          (kbd "M-l") 'org-metaright
-          (kbd "M-h") 'org-metaleft
-          (kbd "M-k") 'org-metaup
-          (kbd "M-j") 'org-metadown
-          (kbd "M-L") 'org-shiftmetaright
-          (kbd "M-H") 'org-shiftmetaleft
-          (kbd "M-K") 'org-shiftmetaup
-          (kbd "M-J") 'org-shiftmetadown
-          (kbd "M-o") '(lambda () (interactive)
-                         (evil-org-eol-call
-                          '(lambda()
-                             (org-insert-heading)
-                             (org-metaright))))
-          (kbd "M-t") '(lambda () (interactive)
-                         (evil-org-eol-call
-                          '(lambda()
-                             (org-insert-todo-heading nil)
-                             (org-metaright))))
-          ))
-      '(normal insert))
+
+
+
+
+
+;TODO! (defun always-insert-item ()
+;TODO!   "Force insertion of org item"
+;TODO!   (if (not (org-in-item-p))
+;TODO!       (insert "\n- ")
+;TODO!     (org-insert-item)))
+;TODO! 
+;TODO! (defun evil-org-eol-call (fun)
+;TODO!   "Go to end of line and call provided function"
+;TODO!   (end-of-line)
+;TODO!   (funcall fun)
+;TODO!   (evil-append nil))
+
+;TODO! ;; normal state shortcuts
+;TODO! (evil-define-key 'normal evil-org-mode-map
+;TODO!  "gh" 'outline-up-heading
+;TODO!  "gj" (if (fboundp 'org-forward-same-level) ;to be backward compatible with older org version
+;TODO!           'org-forward-same-level
+;TODO!         'org-forward-heading-same-level)
+;TODO!  "gk" (if (fboundp 'org-backward-same-level)
+;TODO!           'org-backward-same-level
+;TODO!         'org-backward-heading-same-level)
+;TODO!  "gl" 'outline-next-visible-heading
+;TODO!  "t" 'org-todo
+;TODO!  "T" '(lambda () (interactive) (evil-org-eol-call (lambda() (org-insert-todo-heading nil))))
+;TODO!  "H" 'org-beginning-of-line
+;TODO!  "L" 'org-end-of-line
+;TODO!  ";t" 'org-show-todo-tree
+;TODO!  "o" '(lambda () (interactive) (evil-org-eol-call 'always-insert-item))
+;TODO!  "O" '(lambda () (interactive) (evil-org-eol-call 'org-insert-heading))
+;TODO!  "$" 'org-end-of-line
+;TODO!  "^" 'org-beginning-of-line
+;TODO!  "<" 'org-metaleft
+;TODO!  ">" 'org-metaright
+;TODO!  ";a" 'org-agenda
+;TODO!  "-" 'org-cycle-list-bullet
+;TODO!  (kbd "TAB") 'org-cycle)
+
+;TODO! ;; normal & insert state shortcuts.
+;TODO! (mapc (lambda (state)
+;TODO!         (evil-define-key state evil-org-mode-map
+;TODO!           (kbd "M-l") 'org-metaright
+;TODO!           (kbd "M-h") 'org-metaleft
+;TODO!           (kbd "M-k") 'org-metaup
+;TODO!           (kbd "M-j") 'org-metadown
+;TODO!           (kbd "M-L") 'org-shiftmetaright
+;TODO!           (kbd "M-H") 'org-shiftmetaleft
+;TODO!           (kbd "M-K") 'org-shiftmetaup
+;TODO!           (kbd "M-J") 'org-shiftmetadown
+;TODO!           (kbd "M-o") '(lambda () (interactive)
+;TODO!                          (evil-org-eol-call
+;TODO!                           '(lambda()
+;TODO!                              (org-insert-heading)
+;TODO!                              (org-metaright))))
+;TODO!           (kbd "M-t") '(lambda () (interactive)
+;TODO!                          (evil-org-eol-call
+;TODO!                           '(lambda()
+;TODO!                              (org-insert-todo-heading nil)
+;TODO!                              (org-metaright))))
+;TODO!           ))
+;TODO!       '(normal insert))
 
 (provide 'evil-org)
 ;;; evil-org.el ends here
